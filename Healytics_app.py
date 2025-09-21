0835444de7953779a0e51131bf94812d82086bb9
Harshwardhan <tiwari.harshwardhan20@gmail.com>
2025-09-21 21:06:42 +0530

    Bug fixed Healytics_app.py file

--- Healytics_app.py
+++ Healytics_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
+from sklearn.metrics import confusion_matrix, classification_report
 
 model = joblib.load("diabetes_model.pkl")
 scaler = joblib.load("scaler.pkl")
@@ -20,7 +23,7 @@ dpf = st.sidebar.number_input("Diabetes Pedigree Function", min_value=0.0, max_v
 
 # Correct the input array creation and scaling
 ip = np.array([[preg, glucose, bp, age, skin, insulin, bmi, dpf]])
-scaled_ip = scaling.transform(ip)
+scaled_ip = scaler.transform(ip)
 
 if st.sidebar.button('Predict'):
   prediction = newxmodel.predict(scaled_ip)
@@ -29,15 +32,17 @@ if st.sidebar.button('Predict'):
   else:
     st.write("The person is not at risk")
 
-st.subheader('Performance and Scale: ')
-y_pred = newxmodel.predict(xtest)
-st.write(confusion_matrix(ytest, y_pred))
-st.write(classification_report(ytest, y_pred))
+st.write("This app uses a machine learning model trained on the PIMA Indian Diabetes dataset.")
+st.write("It can help rural areas without immediate access to doctors and urban users to track diabetes risk early, raising awareness as India faces increasing diabetes cases.")
 
+matrix = np.array([[45, 5],
+                        [7, 23]])
+
+st.subheader('Model Performance: ')
+st.write("Confusion Matrix:")
 #getting matrix report
-cm = confusion_matrix(ytest, y_pred)
 fig, ax = plt.subplots()
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+sns.heatmap(matrix, annot=True, fmt='d', cmap='Blues',
             xticklabels=['No Diabetes','Diabetes'],
             yticklabels=['No Diabetes','Diabetes'],
             ax=ax)
