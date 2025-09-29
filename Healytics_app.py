eb2c2cbf70cb69045af0d20388d17d022cd64468
Harshwardhan. <harshwardhantiwari007@gmail.com>
2025-09-29 11:35:31 +0530

    Update Healytics_app.py
    Risk classification into 3 categories

--- Healytics_app.py
+++ Healytics_app.py
@@ -37,12 +37,41 @@ scaled_ip = scaling.transform(ip)
 if st.sidebar.button('Predict'):
   prediction = newxmodel.predict(scaled_ip)
   probability = newxmodel.predict_proba(scaled_ip)
-
-  prob_text = f"Likelihood of Diabetes: {probability[0][1]*100:.2f}%"
+  prob = probability[0][1]
+  prob_text = f"Likelihood of Diabetes: {prob*100:.2f}%"
   st.write("Prediction:", "Diabetic" if prediction[0] == 1 else "Non-Diabetic")
   st.success(prob_text)
-  
-  if prediction[0] == 1:
+
+  if prob < 0.3:
+    risk_level = "Low Risk"
+    color = "green"
+    st.markdown(f"<p style='color:green; font-size:20px;'> Low Risk : <b>{prob_text}</b></p>", unsafe_allow_html=True)
+    st.markdown("<h3>Recommendations:</h3>", unsafe_allow_html=True)
+    st.write("- Keep maintaining a healthy lifestyle.")
+    st.write("- Regular checkups once or twice a year.")
+    st.write("- Exercise and balanced diet help prevent future risk.")
+    
+  elif prob < 0.6:
+    st.markdown(f"<p style='color:orange; font-size:20px;'> Moderate Risk : <b>{prob_text}</b></p>", unsafe_allow_html=True)
+    risk_level = "Moderate Risk"
+    color = "orange"
+    st.markdown("<h3>Recommendations:</h3>", unsafe_allow_html=True)
+    st.write("- Monitor blood glucose regularly.")
+    st.write("- Maintain a balanced diet and exercise routinely.")
+    st.write("- Consult a doctor if risk factors increase.")
+  else:
+    st.markdown(f"<p style='color:red; font-size:20px;'>Person is Prone to diabetes: <b>{prob_text}</b></p>", unsafe_allow_html=True)
+    risk_level = "High Risk"
+    color = "red"
+    st.markdown("<h3>Recommendations:</h3>", unsafe_allow_html=True)
+    st.write("- Consult a doctor for a proper checkup immediately.")
+    st.write("- Maintain a healthy diet (low sugar, balanced meals).")
+    st.write("- Exercise regularly (30 mins/day).")
+    st.write("- Monitor blood glucose levels frequently.")
+    
+  '''
+  '''
+  if prob >:
     st.markdown(f"<p style='color:red; font-size:20px;'>Person is Prone to diabetes: <b>{prob_text}</b></p>", unsafe_allow_html=True)
     st.markdown("<h3>Recommendations:</h3>", unsafe_allow_html=True)
     st.write("- Consult a doctor for a proper checkup.")
@@ -55,6 +84,7 @@ if st.sidebar.button('Predict'):
     st.write("- Keep maintaining a healthy lifestyle.")
     st.write("- Regular checkups once or twice a year.")
     st.write("- Exercise and balanced diet help prevent future risk.")
+  '''
 
 st.write("This app uses a machine learning model trained on the PIMA Indian Diabetes dataset.")
 st.write("Continuous health tracking: Users can monitor their health regularly by integrating fitness trackers, diet plans, and lifestyle monitoring apps, enabling proactive diabetes management.")
